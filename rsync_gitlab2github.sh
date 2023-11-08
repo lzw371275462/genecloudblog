63e17a41eced80acc2760f69bcba266632058ad2
luozw <luozw@geneplus.org.cn>
2023-11-08 09:39:05 +0800

    add first gitlab version

--- rsync_gitlab2github.sh
+++ rsync_gitlab2github.sh
@@ -1,3 +1,3 @@
-rsync -avzP /mnt/GeneCloud/luozw/git/genecloudblog_gitlab/build/ /mnt/GeneCloud/luozw/git/genecloudblog_github/build/
-rsync -avzP /mnt/GeneCloud/luozw/git/genecloudblog_gitlab/source/ /mnt/GeneCloud/luozw/git/genecloudblog_github/source/
+rsync -avzP --delete /mnt/GeneCloud/luozw/git/genecloudblog_gitlab/build/ /mnt/GeneCloud/luozw/git/genecloudblog_github/build/
+rsync -avzP --delete /mnt/GeneCloud/luozw/git/genecloudblog_gitlab/source/ /mnt/GeneCloud/luozw/git/genecloudblog_github/source/
 
--- source/多组学平台说明文档/index.rst
+++
@@ -1,9 +0,0 @@
-多组学平台说明文档 
-=================================
- 
- .. toctree::
-            :maxdepth: 1
-
-    SingleCellPlatform/contents
-    RNAPlatform/contents
-
